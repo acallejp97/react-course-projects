972aa8a752810d86617bb8b18903fd8d864fc828
Asier Callejo <acallejo@bilbomatica.es>
2021-09-22 00:30:59 +0200

    Exercised until lesson 12

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,14 +1,177 @@
 "use strict";
 
 console.log("App.js is running!");
-
-// JSX - JavaScript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "Does this change?"
-);
-
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+// JSX - JavaScript XML
+
+//Template
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Indecision app"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
+);
+
+// ReactDOM.render(template, appRoot);
+
+//Dinamic template
+var user = {
+  name: "Asier Callejo",
+  age: 24,
+  location: "Bilbao, Spain"
+};
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    user.location
+  )
+);
+
+// ReactDOM.render(templateTwo, appRoot);
+
+//Challenge
+var app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer"
+};
+
+var templateThree = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.subtitle
+  )
+);
+
+// ReactDOM.render(templateThree, appRoot);
+
+var user = {
+  name: "Asier Callejo",
+  age: 24,
+  location: "Bilbao, Spain"
+};
+
+function getLocation(location) {
+  return location ? React.createElement(
+    "p",
+    null,
+    "Location: ",
+    location
+  ) : undefined;
+}
+
+var templateFour = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name ? user.name : "Anonymous"
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+// ReactDOM.render(templateFour, appRoot);
+
+var app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"]
+};
+
+function getValues(array) {
+  var returnable = [];
+  for (var index = 0; index < array.length; index++) {
+    var element = React.createElement(
+      "li",
+      null,
+      "Item ",
+      array[index]
+    );
+    returnable.push(element);
+  }
+  return returnable;
+}
+
+var templateFive = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length ? "Here are your options" : "No options"
+  ),
+  app.options.length > 0 && React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "ol",
+      null,
+      getValues(app.options)
+    )
+  )
+);
+
+ReactDOM.render(templateFive, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,100 @@
 console.log("App.js is running!");
-
-// JSX - JavaScript XML
-var template = <h1>Does this change?</h1>;
-
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+// JSX - JavaScript XML
+
+//Template
+var template = (
+  <div>
+    <h1>Indecision app</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+// ReactDOM.render(template, appRoot);
+
+//Dinamic template
+var user = {
+  name: "Asier Callejo",
+  age: 24,
+  location: "Bilbao, Spain",
+};
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
+
+// ReactDOM.render(templateTwo, appRoot);
+
+//Challenge
+var app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer",
+};
+
+var templateThree = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+);
+
+// ReactDOM.render(templateThree, appRoot);
+
+var user = {
+  name: "Asier Callejo",
+  age: 24,
+  location: "Bilbao, Spain",
+};
+
+function getLocation(location) {
+  return location ? <p>Location: {location}</p> : undefined;
+}
+
+var templateFour = (
+  <div>
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
+// ReactDOM.render(templateFour, appRoot);
+
+var app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer",
+  options: ["One", "Two"],
+};
+
+function getValues(array) {
+  var returnable = [];
+  for (let index = 0; index < array.length; index++) {
+    var element = <li>Item {array[index]}</li>;
+    returnable.push(element);
+  }
+  return returnable;
+}
+
+var templateFive = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length ? "Here are your options" : "No options"}</p>
+    {app.options.length > 0 && (
+      <div>
+        <ol>{getValues(app.options)}</ol>
+      </div>
+    )}
+  </div>
+);
+
+ReactDOM.render(templateFive, appRoot);
